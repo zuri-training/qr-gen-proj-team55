e81603e9ab5b2b3ad7d1f065bbbcc6864db5afac
Joshua <ezejoshuac@gmail.com>
2022-08-01 18:31:53 +0100

    Static - Created varibles for serving static files
    Templates - Made root directory templates accessible across apps

--- qr_gen/qr_gen/settings.py
+++ qr_gen/qr_gen/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = "qr_gen.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [ BASE_DIR / "templates", ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
