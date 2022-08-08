6326e1e9837a052e1f9607a2c1b63224ade4157f
Mfirs <mfirstborn@gmail.comgit config --global user.name Mfirs  git config --global user.email mfirstborn@gmail.com>
2022-08-08 18:25:32 +0200

    - renamed the images link in landingpage - updated requirements.txt commented out ststicfiles_storage updated urls.py

--- qr_gen/qr_gen/urls.py
+++ qr_gen/qr_gen/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("authentication.urls")),
-    path("home/", include("qr_gen_app.urls")),
-]
+    path("", include("qr_gen_app.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
