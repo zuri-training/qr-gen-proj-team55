276c1ba3e27c003c4aa9e2da6249e548c429ed25
drizla01 <awal4mail@yahoo.com>
2022-08-09 17:47:55 +0100

    update profile, signup, model in authentications

--- qr_gen/authentication/models.py
+++ qr_gen/authentication/models.py
@@ -23,11 +23,12 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
 
+
 class Profile(models.Model):
     email = models.ForeignKey(CustomUser, max_length=120, on_delete=models.CASCADE)
     name = models.CharField(max_length=120, null=True)
     phone_Number = models.CharField(max_length=120, null=True)
-    avater = models.ImageField(null=True, default="avatar.svg")
+    avater = models.ImageField(null=True, default="")
     bio = models.TextField(null=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
