a6759429303607aa35853bac81dcd074b35fd783
Digho007 <dighomanorjeremiah@gmail.com>
2022-08-10 19:58:59 +0100

    qr code functionality

--- qr_gen/qr_gen_app/models.py
+++ qr_gen/qr_gen_app/models.py
@@ -9,7 +9,7 @@ from authentication.models import CustomUser
 
 class BaseModel(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    QRcode_name = models.CharField(max_length=255)
     url = models.URLField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
