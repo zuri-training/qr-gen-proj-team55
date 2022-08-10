47e1a32da46021b4db0fb56ba92b7bf6a5a2c040
Digho007 <dighomanorjeremiah@gmail.com>
2022-08-10 18:39:13 +0100

    qr code display

--- qr_gen/qr_gen_app/views.py
+++ qr_gen/qr_gen_app/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import QrcodeBusiness, QrcodeAppDownload, QrcodeEvent
+from authentication.models import CustomUser
+
 
 class qr_dashboardview(TemplateView):
     template_name = 'qr_gen_app/maindashboard.html'
 
 class appdownload(TemplateView):
     template_name = 'qr_gen_app/sdashboard.html'
+
+
+def QrcodeBusiness_view(request):
+    name = "Here is your QR code"
+
+    obj = QrcodeBusiness.objects.get(fk=CustomUser)
+
+    context = {
+        'name': name,
+        'obj' : obj,
+    }
+
+    return render(request, 'qr_gen_app/qrcode_view.html', context)
+
+
