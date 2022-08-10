1a21d33f2bed6a3a27965888fdeeb3078a43520b
David Ilori <ddluwole@gmail.com>
2022-08-10 03:50:21 +0100

    Connected the dashboard3

--- qr_gen/qr_gen_app/urls.py
+++ qr_gen/qr_gen_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'qr_gen_app'
+
 urlpatterns = [
-    path("",views.landing_page, name='home')
+    path("",views.landing_page, name='home'),
+    path("dashboard/",views.qr_dashboardview.as_view(), name='qr_dashboard'),
+    path("dashboard3/",views.qr_dashboard3view, name='qr_dashboard3'),
 ]
--- qr_gen/qr_gen_app/views.py
+++ qr_gen/qr_gen_app/views.py
@@ -1,5 +1,48 @@
-from django.shortcuts import render
-from .models import qrcode_business, qrcode_appDownload, qrcode_event
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from django.utils.safestring import mark_safe
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
+
+
+
+class qr_dashboardview(TemplateView):
+    template_name = 'qr_gen_app/maindashboard.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['qrcode_business'] = qrcode_business.objects.all()
+    #     context['qrcode_appDownload'] = qrcode_appDownload.objects.all()
+    #     context['qrcode_event'] = qrcode_event.objects.all()
+    #     return context
+    
+
+
+
+
+@login_required
+def qr_dashboard3view(request):
+    
+    if request.method =='POST':
+        company= request.POST.get('company_name', None)
+        about = request.POST.get('about', None)
+        opening_hours = request.POST.get('opening_hours', None)
+        contact_details = request.POST.get('contact_details', None)
+        web_url = request.POST.get('web_url', None)
+
+        return redirect ('dashborard3.html')
+
+    return render(request, 'qr_gen_app/dashboard3.html')
+
+
+
+
+
+
+
 
 def landing_page(request):
     return render(request, "qr_code_app/LandingPage.html")
