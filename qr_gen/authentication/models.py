181b4938de319d0dc6f194378c0636cc553766b4
Joshua <ezejoshuac@gmail.com>
2022-08-11 15:44:37 +0100

    Removed tkinter import

--- qr_gen/authentication/models.py
+++ qr_gen/authentication/models.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 # from tkinter import CASCADE
->>>>>>> 5c7c499 (Modified: Auth model)
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
