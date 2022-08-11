37c3d16accf7ffd76a79c491bc4f764d6595f7f1
Digho007 <dighomanorjeremiah@gmail.com>
2022-08-11 23:20:39 +0100

    Arrangements

--- qr_gen/qr_gen_app/urls.py
+++ qr_gen/qr_gen_app/urls.py
@@ -6,6 +6,6 @@ app_name = "qr_gen_app"
 urlpatterns = [
     path('qr_dashboard/', views.qr_dashboardview.as_view(), name='qr_dashboard'),
     path('appdownload/', views.appdownload.as_view(), name='appdownload'),
-    
+    path('qrcode/', views.save_QrcodeAppDownload, name='save_code'),
      
 ]
--- qr_gen/qr_gen_app/views.py
+++ qr_gen/qr_gen_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from .models import QrcodeBusiness, QrcodeAppDownload, QrcodeEvent
 from authentication.models import CustomUser
@@ -14,7 +14,7 @@ class appdownload(TemplateView):
     template_name = 'qr_gen_app/sdashboard.html'
 
 
-def QrcodeBusiness_view(request):
+def Qrcode_view(request):
     name = "Here is your QR code"
 
     obj = QrcodeBusiness.objects.get(fk=CustomUser)
@@ -23,19 +23,16 @@ def QrcodeBusiness_view(request):
         'name': name,
         'obj' : obj,
     }
-    return render(request, 'qr_gen_app/QrcodeBusiness_view.html', context)
+    return render(request, 'qr_gen_app/Qrcode_view.html', context)
 
 def save_QrcodeAppDownload(request):
     if request.method == "POST":
-        name = request.POST.get("name")
-        link = request.POST.get("link")
-        company = request.POST.get("company")
-        about = request.POST.get("about")
-        hours = request.POST.get("hours")
-        address = request.POST.get("address")
-        email = request.POST.get("email")
-        phone = request.POST.get("phone")
-        QrcodeBusiness = QrcodeBusiness(QRcode=name,url=link,company=company, about=about, opening_hours=hours,address=address,email=email,phone=phone)
-        QrcodeBusiness.save()
-    return render (request, "QrcodeBusiness_view.html")
+        Aname = request.POST.get("APPname")
+        description = request.POST.get("desc")
+        url = request.POST.get("url")
+        Qname = request.POST.get("QRcode")
+        Inst_1 = QrcodeAppDownload(QRcode_name=Qname,url=url,app_name=Aname,description=description)
+        Inst_1.save()
+        return redirect ('/qrcode')
+    return render (request, "qr_gen_app/sdashboard.html")
 
