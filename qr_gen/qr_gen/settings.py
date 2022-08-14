286df056faa2ea364d80c19b322468db99b5096e
drizla01 <awal4mail@yahoo.com>
2022-08-14 20:04:05 +0100

    updated requirement

--- qr_gen/qr_gen/settings.py
+++ qr_gen/qr_gen/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_URL = "/images/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
