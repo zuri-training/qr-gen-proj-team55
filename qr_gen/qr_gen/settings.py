1eb014650a22721c1ca25ab2fd3c1104c6748f61
drizla01 <awal4mail@yahoo.com>
2022-08-14 19:50:54 +0100

    configure whitenoise

--- qr_gen/qr_gen/settings.py
+++ qr_gen/qr_gen/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_URL = "/images/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
