ab8aa78bc098b8eb622b52939f257cd7e661e485
Awal Umar <76787760+drizla01@users.noreply.github.com>
2022-08-14 22:26:38 +0100

    Update settings.py

--- qr_gen/qr_gen/settings.py
+++ qr_gen/qr_gen/settings.py
@@ -77,7 +77,14 @@ WSGI_APPLICATION = "qr_gen.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+'''
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.sqlite3',
+      'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
+   }
+}
+'''
 DATABASES={
    'default':{
       'ENGINE':'django.db.backends.postgresql_psycopg2',
